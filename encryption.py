d030e7f96beb5235ea70185e7286afabeeefa896
calvin <Calvinoj19@gmail.com>
2024-07-06 18:50:15 -0600

    working basic product see comments

--- encryption.py
+++ encryption.py
@@ -1,49 +1,60 @@
 import pickle
+# install cryptography package/lib
+from cryptography.fernet import Fernet
+import hashlib
 
 
-def classifydata(data):
-    print(data)
-    type = data[0]
-    print(type)
+# the DHS class just makes a list that will hold 'pairs' or just a hashed version of the encryption key and the
+# encrypted password so that they can be found later in the list when supplied with the key
+class DHS:
 
-    if 'card info' in type:
-        rawcardinfo = info[1]
-        print(rawcardinfo)
+    def __init__(self):
+        self._lst = []
 
+    # used to encrypt a supplied password add it to the list then return the encryption key
+    # Note: Fernet.encrypt and this encrypts are different methods same with decrypt
+    def encrypt(self, password) -> bytes:
+        # generate key
+        key = Fernet.generate_key()
+        # create an instance of the key
+        fernet = Fernet(key)
+        # encrypt the password(password has to be encoded first to work idk)
+        encrypted_password = fernet.encrypt(password.encode())
+        # create a new pair with hashed key and encrypted password then add it to the list
+        self._lst.append(Pair(hash(key), encrypted_password))
+        return key
 
-    else:
-        print('test')
+    # decrypt and return the password
+    def decrypt(self, key):
+        return Fernet(key).decrypt(name.get(key)).decode()
 
-
-
-
-
-
-
-
-
-#encryption methods
-
-
-
-
-
-
-
-
-
-
+    # gets the value at a specific key hash location
+    def get(self, key):
+        h = hash(key)
+        for x in self._lst:
+            if h == x.get_key():
+                return x.get_value()
 
 
+# creates a tuple of a key value pair which is then stored in the DHS class
+class Pair:
 
+    def __init__(self, key, value):
+        self.pair = (key, value)
 
+    def get_key(self):
+        return self.pair[0]
 
+    def get_value(self):
+        return self.pair[1]
+
+    def __str__(self):
+        return f'{self.pair[0]}, {self.pair[1]}'
 
 
 if __name__ == '__main__':
-    with open('data.pickle', 'rb') as f:
-        info = pickle.load(f)
-        info = info
-    classifydata(info)
-
-
+    name = DHS()
+    key = name.encrypt(input("enter a password: "))
+    print(f"Encryption key: {key}")
+    print(f"Encrypted password: {name.get(key)}")
+    print(f"Decrypting password: {name.decrypt(key)}")
